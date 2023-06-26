c8bae3c1fab249ca80ee6144ca677b0d4c4b315c
SantiagoAlguien <snlopezl@rtisas.com.co>
2023-06-26 17:55:35 -0500

    parte 1 del capitulo 6

--- helllo_word/lib/main.dart
+++ helllo_word/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:helllo_word/Pagina02.dart';
 
 void main() => runApp(const MiApp());
 
@@ -9,11 +8,13 @@ class MiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title:"Mi primera App",
-      home: Inicio(), //Este constructor es el que llama a la class Inicio extends StatefulWidget que inicia todo el Scafold
+      title: "Mi primera App",
+      home:
+          Inicio(), //Este constructor es el que llama a la class Inicio extends StatefulWidget que inicia todo el Scafold
     );
   }
 }
+
 class Inicio extends StatefulWidget {
   const Inicio({super.key});
   @override
@@ -24,27 +25,40 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Navegacion"),
-      ), //Barra superior de navegacion 
-
+      appBar: AppBar(
+        title: Text("AlertDialog"),
+      ), //Barra superior de navegacion
       body: Center(
-      child : Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:<Widget>[
-          Text("Hola"
-          ),
-          ElevatedButton( //La palabra RaisedBottun() quedo obsoleta 
-            child: Text("Ir a otra pagina "),
-            onPressed: ()=>{
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context)=>Pagina02())
-                )
-            }
-            )
-        ],
-      ), 
-    ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text("Mostrar"),
+              onPressed: () {
+                mostrarAlerta(context);
+              },
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Text("si"),
+          ],
+        ),
+      ),
     );
   }
+}
+
+void mostrarAlerta(BuildContext context) {
+  //Funcion que va a contener nuestra alerta
+  showDialog(
+    barrierDismissible: true,
+    context: context,
+    builder: (context){
+      return AlertDialog(
+        title: Text("hola"),
+        content: Text("¿Como estas ?"),        
+      );
+    }
+  );
 }
