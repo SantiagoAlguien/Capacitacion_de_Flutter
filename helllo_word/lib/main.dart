90831a3ad7ec617b58b2248abcf6edac4a529f8a
SantiagoAlguien <snlopezl@rtisas.com.co>
2023-07-03 19:25:14 -0500

    Inicio Capitulo Consumiendo un API REST ≫ Listas

--- helllo_word/lib/main.dart
+++ helllo_word/lib/main.dart
@@ -1,46 +1,21 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp( MiApp());
-
 class MiApp extends StatelessWidget {
-  
-Empresa _facebook = new Empresa (
-  "Facebook", "Marck", 50000
-);
-
-Text _nombre = new Text("Hernesto",style: TextStyle(fontSize: 40),);
-
-MiApp({super.key});
-@override
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: MaterialApp(
-        title: "Clases",
+        title: "Material app",
         home: Scaffold(
           appBar: AppBar(
-            title: Text("Clases"),
+            title: Text("Material app Bar"),
           ),
-          body: Center(
-            child: Text(
-              _facebook.ingresoAnu.toString()
-            )
-            
-          ),
+          body:Center(
+            child: Container(
+              child: Text("hello world"),
+          ),)
         ),
       ),
     );
   }
-}
-
-class Empresa {
-  String? nombre;
-  String? propietario;
-  int? ingresoAnu;
-
-   Empresa(String? nom, String? pro, int? ing){
-    nombre = nom;
-    propietario = pro;
-    ingresoAnu = ing;
-
-   }
 }
